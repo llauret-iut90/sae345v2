ec85b19b48a8b60667357d4f36b7671f36f2fbc3
llauret <lucas.lauret@edu.univ-fcomte.fr>
2023-03-28 10:16:55 +0200

    distinct

--- controllers/admin_dataviz.py
+++ controllers/admin_dataviz.py
@@ -12,21 +12,19 @@ admin_dataviz = Blueprint('admin_dataviz', __name__,
 @admin_dataviz.route('/admin/dataviz/etat1')
 def show_type_article_stock():
     mycursor = get_db().cursor()
-    sql = '''
-SELECT ts.libelle_ski as type_ski,
+    sql = '''SELECT ts.libelle_ski as type_ski,
        ts.id_type_skis,
        s.libelle_skis AS nom_skis,
        GROUP_CONCAT(s.libelle_skis) as skis,
-       IFNULL(ROUND(AVG(n.note),2), 0) AS moyenne_note, 
-       COUNT(n.Id_skis) AS nb_notes, 
-       COUNT(c.Id_skis) AS nbr_commentaires
+       IFNULL(ROUND(AVG(DISTINCT n.note),2), 0) AS moyenne_note, 
+       COUNT(DISTINCT n.note) AS nb_notes, 
+       COUNT(DISTINCT c.commentaire) AS nbr_commentaires
 FROM skis s
 INNER JOIN type_skis ts ON s.type_skis_id = ts.id_type_skis
 LEFT JOIN note n ON s.code_ski = n.Id_skis
 LEFT JOIN commentaire c ON s.code_ski = c.Id_skis
 GROUP BY ts.libelle_ski, ts.id_type_skis, s.libelle_skis
-ORDER BY ts.id_type_skis,moyenne_note DESC;
-
+ORDER BY ts.id_type_skis, moyenne_note DESC;
            '''
     mycursor.execute(sql)
     datas_show = mycursor.fetchall()
